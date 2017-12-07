e2ea23a232374a8f2bc45d2065c561e512ff643c
Hryts Jakovytch Varenyk <wsa@localhost.localdomain>
2017-12-07 19:45:08 +0200

    add products data in item

---
+++ data/products.js
@@ -0,0 +1,341 @@
+var Products = {
+  "58e2a5433614230c68a160d2": {
+     _id: "",
+    name: 'Sunflowers In Jar',
+    description: 'Altona is the sunflower capitol of the Canada!  Here\'s the perfect arrangement to represent our landscape!  Container and selection of additional flowers may vary.  We will contact you with a substitution if sunflowers are unavailable. ',
+    category: 'simple bouquets for your friends and family',
+    cvrImg: 'media/s119415362602402877_p38_i1_w340.jpeg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5433614230c68a160d1",
+      price: 49,
+      __v: 1,
+      imageUrls: [
+        'media/s119415362602402877_p38_i1_w340.jpeg',
+        'media/HW0_372559.jpg',
+        'media/Pastel+2.jpg',
+        'media/gerbera.jpg',
+        'media/multi+rose.jpg'
+      ]
+    }]
+  },
+  "58e2a5433614230c68a160d4": {
+     _id: "",
+    name: 'Bold & Beautiful Flowers',
+    description: 'Beautifully arranged vibrant warm tones with a  variety of roses, carnations and daisies.  Arranged in a vase or cut flower bouquet.  Flowers and container may vary.  ',
+    category: 'simple bouquets for your friends and family',
+    cvrImg: 'media/HW0_372559.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{ _id: "58e2a5433614230c68a160d3",
+      price: 35,
+      __v: 0,
+      imageUrls: [ 'media/HW0_372559.jpg' ]
+    }]
+  },
+  "58e2a5433614230c68a160d6": {
+     _id: "",
+    name: 'Bright and Cheery',
+    description: 'Sure to brighten a room, this arrangement features lilies and a variety of fresh pinks, yellows and greens.  Arranged in a vase or order as a cut flower bouquet.  Container and floral selection may vary.',
+    category: 'simple bouquets for your friends and family',
+    cvrImg: 'media/Pastel+2.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{ _id: "58e2a5433614230c68a160d5",
+      price: 11,
+      __v: 0,
+      imageUrls: [ 'media/Pastel+2.jpg' ]
+    }]
+  },
+  "58e2a5433614230c68a160d8": {
+     _id: "",
+    name: 'Gerbera Arrangement',
+    description: 'Beautiful variety of the ever popular Gerbera Daisies arranged in a vase or hand tied bouquet.  Flower color and vase may vary on availabilty.',
+    category: 'simple bouquets for your friends and family',
+    cvrImg: 'media/gerbera.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5433614230c68a160d7",
+      price: 22,
+      __v: 0,
+      imageUrls: [ 'media/gerbera.jpg' ]
+    }]
+  },
+  "58e2a5433614230c68a160da": {
+     _id: "",
+    name: 'Roses in a Vase',
+    description: 'Love roses?  Variety of roses arranged in a vase with just the perfect amount of extras to make sure the roses steal the show.  Available in Modest (6 roses) Deluxe (12 Roses) Premier (18 Roses) or Grand (24 Roses)  Colors and vase may vary based on availablity.  ',
+    category: 'simple bouquets for your friends and family',
+    cvrImg: 'media/multi+rose.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5433614230c68a160d9",
+      price: 45,
+      __v: 1,
+      imageUrls: [
+        'media/multi+rose.jpg',
+        'media/9343.jpg',
+        'media/Tropical.jpg'
+      ]
+    }]
+  },
+  "58e2a5443614230c68a160dc": {
+     _id: "",
+    name: 'Gerbera\'s in a stem vase',
+    description: 'Gerbera Daisies arranged in a stem vase with filler and greens.  Color of flowers and stem vase may vary. ',
+    category: 'simple bouquets for your friends and family',
+    cvrImg: 'media/9343.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160db",
+      price: 38,
+      __v: 0,
+      imageUrls: [ 'media/9343.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160de": {
+     _id: "",
+    name: 'Tropical Arrangement',
+    description: 'Flowers with a tropical touch, arranged in a vase!  Flowers and container may vary from photo.',
+    category: 'simple bouquets for your friends and family',
+    cvrImg: 'media/Tropical.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160dd",
+      price: 56,
+      __v: 0,
+      imageUrls: [ 'media/Tropical.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160e1": {
+     _id: "",
+    name: 'Variety of Flowers',
+    description: 'Assorted flowers in a pastel palette, or bold and bright colors, beautifully arranged in a vase.  Modest $50, Deluxe $80, Premier $100, Grand $140.  Variety of flowers and container may vary.',
+    category: 'truly unique bouquets for one and only beloved',
+    cvrImg: 'media/9343.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{ _id: "58e2a5443614230c68a160e0",
+      price: 49,
+      __v: 1,
+      imageUrls: [
+        'media/9343.jpg',
+        'media/rose+bouquet.jpg',
+        'media/roses.jpg',
+        'media/Single+Rose.jpg'
+      ]
+    }]
+  },
+  "58e2a5443614230c68a160e3": {
+     _id: "",
+    name: 'Wrapped Rose Bouquet',
+    description: 'Cut roses carefully arranged in a hand tied bouquet.  Available in Modest (6 Roses) $40, Deluxe (12 Roses) $75, Premier (18 Roses)$115, and Grand (24 Roses) $145.  Variety of Colors available for you to choose from, based on current supply.',
+    category: 'truly unique bouquets for one and only beloved',
+    cvrImg: 'media/rose+bouquet.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160e2",
+      price: 34,
+      __v: 0,
+      imageUrls: [ 'media/rose+bouquet.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160e5": {
+     _id: "",
+    name: 'Box of Rose Petals',
+    description: 'Boxed rose petals to add to your romantic setting $20.  Variety of colors available for you to choose from, based on current supply',
+    category: 'truly unique bouquets for one and only beloved',
+    cvrImg: 'media/Rose+Petals.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160e4",
+      price: 21,
+      __v: 0,
+      imageUrls: [ 'media/Rose+Petals.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160e7": {
+     _id: "",
+    name: 'Rose Arrangement',
+    description: 'Quality Roses beautifully arranged in a vase.  Available in Modest (6 Roses) $50, Deluxe (12 Roses) $85, Premier (18 Roses) $125 and Grand (24 Roses) $165.  Variety of Colors available for you to choose from, based on current supply.',
+    category: 'truly unique bouquets for one and only beloved',
+    cvrImg: 'media/roses.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160e6",
+      price: 78,
+      __v: 0,
+      imageUrls: [ 'media/roses.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160e9": {
+     _id: "",
+    name: 'Single Rose',
+    description: 'A classic single rose packaged with greens and baby\'s breath. $10. Variety of colors available for you to choose from, based on current supply.',
+    category: 'truly unique bouquets for one and only beloved',
+    cvrImg: 'media/Single+Rose.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160e8",
+      price: 22,
+      __v: 0,
+      imageUrls: [ 'media/Single+Rose.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160eb": {
+     _id: "",
+    name: 'Gerbera Daisies',
+    description: 'The very popular Gerbera Daisies beautifully arranged in a container or wrapped bouquet.  Colors and vase may vary.',
+    category: 'truly unique bouquets for one and only beloved',
+    cvrImg: 'media/gerbera.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160ea",
+      price: 64,
+      __v: 0,
+      imageUrls: [ 'media/gerbera.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160ee": {
+     _id: "",
+    name: 'Elegant White Arrangement',
+    description: 'Artfully arranged assortment of white flowers and with green foliage accents.  Variety of flowers and vase may vary based on availablity.  ',
+    category: 'light and playful bouquets, for expressing sympathy',
+    cvrImg: 'media/white+roses+small.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{ _id: "58e2a5443614230c68a160ed",
+      price: 49,
+      __v: 1,
+      imageUrls: [
+        'media/white+roses+small.jpg',
+        'media/White+Roses.jpg'
+      ]
+    }]
+  },
+  "58e2a5443614230c68a160f0": {
+     _id: "",
+    name: 'Carnations in Vase',
+    description: 'Assortment of carnations arranged in a vase.  ',
+    category: 'light and playful bouquets, for expressing sympathy',
+    cvrImg: 'media/Carns.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160ef",
+      price: 34,
+      __v: 0,
+      imageUrls: [ 'media/Carns.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160f2": {
+     _id: "",
+    name: 'Rose Arrangement',
+    description: 'Classic roses arranged in a vase.  Colors will vary based on availability.  Specific colors may be requested.',
+    category: 'light and playful bouquets, for expressing sympathy',
+    cvrImg: 'media/White+Roses.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160f1",
+      price: 21,
+      __v: 0,
+      imageUrls: [ 'media/White+Roses.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160f4": {
+     _id: "",
+    name: 'Hydrangea Arrangement',
+    description: 'Always beautiful Hydrangea arranged with accent flowers.  Colors and vase may vary based on availablity.',
+    category: 'light and playful bouquets, for expressing sympathy',
+    cvrImg: 'media/blue+and+white.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160f3",
+      price: 78,
+      __v: 0,
+      imageUrls: [ 'media/blue+and+white.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160f7": {
+     _id: "",
+    name: 'Peaceful Lillies',
+    description: 'A lily is a symbol of peace.  Express your sympathy with this beautiful arrangement of lilies and subdued tones.  Flowers and vase may vary based on availablity.',
+    category: 'light and playful bouquets, for expressing sympathy',
+    cvrImg: 'media/pastels.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160f6",
+      price: 22,
+      __v: 0,
+      imageUrls: [ 'media/pastels.jpg' ]
+    }]
+  },
+  "58e2a5443614230c68a160f9": {
+     _id: "",
+    name: 'Designer\'s Choice',
+    description: 'Not sure how you\'d like to express your feelings?  Let our creative experts prepare an arrangement of their choice.  Specific colors or flowers may be requested.',
+    category: 'light and playful bouquets, for expressing sympathy',
+    cvrImg: 'media/Roses+and+snaps.jpg',
+    cvrItem: 0,
+    user: "58e29e9ad2b2db0bff1fe327",
+    __v: 0,
+    items: [{
+      _id: "58e2a5443614230c68a160f8",
+      price: 64,
+      __v: 0,
+      imageUrls: [ 'media/Roses+and+snaps.jpg' ]
+    }]
+  },
+  index: [
+    "58e2a5433614230c68a160d2",
+    "58e2a5433614230c68a160d4",
+    "58e2a5433614230c68a160d6",
+    "58e2a5433614230c68a160d8",
+    "58e2a5433614230c68a160da",
+    "58e2a5443614230c68a160dc",
+    "58e2a5443614230c68a160de",
+    "58e2a5443614230c68a160e1",
+    "58e2a5443614230c68a160e3",
+    "58e2a5443614230c68a160e5",
+    "58e2a5443614230c68a160e7",
+    "58e2a5443614230c68a160e9",
+    "58e2a5443614230c68a160eb",
+    "58e2a5443614230c68a160ee",
+    "58e2a5443614230c68a160f0",
+    "58e2a5443614230c68a160f2",
+    "58e2a5443614230c68a160f4",
+    "58e2a5443614230c68a160f7",
+    "58e2a5443614230c68a160f9"
+  ]
+}
