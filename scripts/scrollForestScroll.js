a92d0fbeebc0ee71de65eb588f896ccf8a8c9e1a
Hryts Jakovytch Varenyk <wsa@localhost.localdomain>
2017-12-07 19:18:51 +0200

    delete local gitignore

---
+++ scripts/scrollForestScroll.js
@@ -0,0 +1,67 @@
+var ScrollForestScroll = {
+  scrollForest: function(elements, listener) {
+    listener.toggle = true;
+    listener.maxScroll = $(document).height() - $(window).height() - 10;
+    listener.currentScroll = undefined;
+    listener.elements = undefined;
+    listener.elements = elements;
+    listener.onscroll = this.onscroll
+    var self = this;
+    $(window).on("scroll", function() {
+      listener.onscroll.call(listener)
+    });
+    $(listener).on("click", this.doScroll);
+  },
+  doScroll: function() {
+    console.log("doScroll", this)
+    this.currentScroll = $("html").get(0).scrollTop;
+    this.maxScroll = $(document).height() - $(window).height() - 10;
+    var self = this;
+    if (this.currentScroll >= this.maxScroll) {
+      $(window).off("scroll");
+      return $('html, body').animate({
+        scrollTop: 0
+      }, 1000, function() {
+        console.log("rollback complete cb: ", this)
+        $(".arrow_rotating").removeClass("arrow_rotated");
+        self.toggle = true;
+        $(window).on("scroll", function() {
+          self.onscroll.call(self)
+        })
+      });
+    } else if (this.currentScroll >= Math.floor(this.elements[this.elements.length-1].offset().top)) {
+      return $('html, body').animate({
+        scrollTop: this.maxScroll
+      }, 500);
+    } else {
+
+      for (var i = 0; i < this.elements.length; i++) {
+        var elOffset = Math.floor(this.elements[i].offset().top)
+        console.log("doScroll", this.currentScroll, elOffset)
+        if (this.currentScroll < elOffset) { //  || currentScroll < elOffset + 7
+          return $('html, body').animate({
+            scrollTop: elOffset
+          }, 500);
+        }
+      }
+      console.log("doScroll", this.currentScroll, elOffset)
+
+    }
+  },
+  onscroll: function() {
+    console.log("scrollingBuddyScrolling", this)
+    if (window.scrollY >= this.maxScroll) {
+      console.log("scrollingBuddyScrolling, window.scrollY >= this.maxScroll")
+      if (this.toggle) {
+        $(".arrow_rotating").addClass("arrow_rotated");
+        this.toggle = false;
+      }
+    } else { // if (window.scrollY <= 10)
+      console.log("scrollingBuddyScrolling,else")
+      if (!this.toggle) {
+        $(".arrow_rotating").removeClass("arrow_rotated");
+        this.toggle = true;
+      }
+    }
+  }
+}
---
+++ scripts/views.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+  var scrollElements = [];
+  $(".section").each(function() {
+    scrollElements.push($(this))
+  });
+
+  if ($(".contacts-section").length != 0) {
+    scrollElements.push($(".contacts-section"));
+  }
+
+  ScrollForestScroll.scrollForest(scrollElements, document.getElementById("arrow-up"));
+
+  frontRouter(function(routing) {
+    $(".go-back_box.arrow-back .go-back_btn_wrap").attr("href", routing.backurl)
+    /*
+    .on("click touchend", function(ev) {
+      ev.preventDefault();
+      console.log(routing.backurl)
+      window.location = routing.backurl
+    })
+    */
+
+    $(".grid .img_wrap").on("click tounchend", function(ev) {
+      ev.preventDefault();
+      var url = $(this).attr("href"); //+ "&" +routing.url_fwd_int;
+      var hash_index = url.search("#")
+      if (hash_index != -1) {
+        var hash = url.slice(hash_index)
+        url = url.slice(0, hash_index)+  "&" +routing.url_fwd_int +hash;
+        console.log(url)
+      } else {
+        url += "&" +routing.url_fwd_int
+        console.log(url)
+      }
+
+      window.location = url;
+    })
+
+    $("#contact").on("click touchend", function(ev) {
+      ev.preventDefault();
+      console.log($(this).attr("href")+ "?" +routing.url_fwd_ext)
+      window.location = $(this).attr("href")+ "?" +routing.url_fwd_ext
+    })
+    console.log(routing)
+  });
+
+  $(window).on("resize", function() {
+    manageHeight();
+  })
+  manageHeight();
+
+})
+
+function manageHeight() {
+  if (window.innerWidth <= 600) {
+    $("#home").css("height", $("#home").css("height"))
+  } else {
+    $("#home").css("height", "100vh")
+  }
+}
